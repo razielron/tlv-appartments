ed7fb58ae7f633383cc0de77c1ff53e93aebc772
razielron <razielron@gmail.com>
2021-10-28 23:55:37 +0300

    wip

--- maps/processLocation.js
+++ maps/processLocation.js
@@ -4,19 +4,43 @@ const {Client} = require("@googlemaps/google-maps-services-js");
 
 const client = new Client({});
 
-client
-  .findPlaceFromText({
-    params: {
-        input: "סוקולוב 39 תל אביב",
-        inputtype: 'textquery',
-        //language: 'iw',
-        key: creds.googleMapsApiKey,
-    },
-    timeout: 1000, // milliseconds
-  })
-  .then((r) => {
-    console.log(r.data);
-  })
-  .catch((e) => {
-    console.log(e.response);
-  });
+async function fun() {
+    let appartmentLoc, res;
+
+    res = await client
+        .findPlaceFromText({
+            params: {
+                input: "דיזינגוף 95 תל אביב",
+                inputtype: 'textquery',
+                language: 'iw',
+                fields: ["geometry", "formatted_address"],
+                key: creds.googleMapsApiKey,
+            },
+            timeout: 1000, // milliseconds
+        });
+
+    console.log(res.data);
+    console.log(res.data.candidates[0].geometry);
+    appartmentLoc = res.data.candidates[0].geometry.location;
+
+    let loc = `${appartmentLoc.lat},${appartmentLoc.lng}`;
+
+    res = await client
+        .textSearch({
+            params: {
+                input: "מכון ספייס",
+                inputtype: 'textquery',
+                language: 'iw',
+                fields: ["geometry", "formatted_address", "place_id"],
+                location: loc,
+                radius: '1000',
+                key: creds.googleMapsApiKey,
+            },
+            timeout: 1000, // milliseconds
+        });
+
+    //console.log(res);
+    console.log(res.data.results);
+}
+
+fun();
